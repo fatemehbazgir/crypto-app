87097da7714f8293bd56a80da0c2b0c6087e637f
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-02-21 14:00:49 +0330

    Styling the table of coins

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,14 +1,16 @@
 import { RotatingLines } from "react-loader-spinner";
+
+import styles from "./TableCoin.module.css";
 import TableRow from "./TableRow";
 
 // Show the coins in the table
 function TableCoin({ coins, isLoading }) {
   return (
-    <div>
+    <div className={styles.container}>
       {isLoading ? (
         <RotatingLines strokeColor="#3874ff" strokeWidth="2" />
       ) : (
-        <table>
+        <table className={styles.table}>
           <thead>
             <tr>
               <th>Coin</th>
--- src/components/modules/TableRow.jsx
+++ src/components/modules/TableRow.jsx
@@ -1,3 +1,5 @@
+import styles from "./TableRow.module.css";
+
 import chartDown from "../../assets/chart-down.svg";
 import chartUp from "../../assets/chart-up.svg";
 
@@ -14,14 +16,16 @@ function TableRow({ coin }) {
   return (
     <tr>
       <td>
-        <div>
+        <div className={styles.symbol}>
           <img src={image} alt={name} />
           <span>{symbol.toUpperCase()}</span>
         </div>
       </td>
       <td>{name}</td>
       <td>${current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}%</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {price_change.toFixed(2)}%
+      </td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
